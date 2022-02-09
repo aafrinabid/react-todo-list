a5f448e379f3a42e707c4b7aed8d2b962acce153
aafrinabid <mohdaafrin@gmail.com>
2022-02-09 13:47:57 +0530

    first react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todolist from './components/Todolist';
+import React from 'react'
+function App() {
+  return (
+    <div className="todo-app">
+    <Todolist />
+    </div>
+  );
+}
+
+export default App;
